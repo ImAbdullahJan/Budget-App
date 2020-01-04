a85d193868dde842c1ae130de29db68e2a791295
Abdullah Jan <abdullahjanazeemi@gmail.com>
2020-01-04 18:54:22 +0500

    Refactor InputField and manage state in parent component.

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,17 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 import { withStyles, Box, TextField, InputLabel } from "@material-ui/core";
 
-const styles = {
+const styles = theme => ({
   textField: {
-    marginTop: 8,
+    marginTop: theme.spacing(1),
     "& .MuiOutlinedInput-input": {
       fontSize: 15
     }
   }
-};
+});
 
-function InputField({ label, classes, fullWidth, placeholder }) {
+function InputField({
+  label,
+  classes,
+  fullWidth,
+  placeholder,
+  value,
+  onChangeValue
+}) {
   return (
     <Box component='span'>
       <InputLabel htmlFor={label}>{label}</InputLabel>
@@ -22,6 +30,8 @@ function InputField({ label, classes, fullWidth, placeholder }) {
         size='small'
         variant='outlined'
         className={classes.textField}
+        value={value}
+        onChange={event => onChangeValue(event)}
       />
     </Box>
   );
@@ -29,6 +39,15 @@ function InputField({ label, classes, fullWidth, placeholder }) {
 
 InputField.defaultProps = {
   fullWidth: true
+};
+
+InputField.propTypes = {
+  label: PropTypes.string,
+  classes: PropTypes.object,
+  fullWidth: PropTypes.bool.isRequired,
+  placeholder: PropTypes.any,
+  value: PropTypes.any.isRequired,
+  onChangeValue: PropTypes.func.isRequired
 };
 
 export default withStyles(styles)(InputField);
