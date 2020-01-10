16e1bc4db5e3ac1241e0a3cf11bab632eacaeb1e
Abdullah Jan <abdullahjanazeemi@gmail.com>
2020-01-10 19:33:53 +0500

    Add previous state in switch case of more than one properties in state.

--- src/contexts/reducer.js
+++ src/contexts/reducer.js
@@ -1,7 +1,7 @@
 export default (state, action) => {
   switch (action.type) {
     case "ADD_ACCOUNT": {
-      return { accounts: [...state.accounts, action.payload] };
+      return { ...state, accounts: [...state.accounts, action.payload] };
     }
 
     default: {
