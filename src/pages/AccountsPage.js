3f484f5d5aa763658a08051e3e95ed05d648777b
Abdullah Jan <abdullahjanazeemi@gmail.com>
2020-01-21 14:14:45 +0500

    Load component with React lazy loading and suspense

--- src/pages/AccountsPage.js
+++ src/pages/AccountsPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback, lazy, Suspense } from "react";
 import _ from "lodash";
 import { useAppState } from "../contexts/AccountContext";
 import { accountsData } from "../api/fakeData";
@@ -6,7 +6,9 @@ import { Box, Grid, Paper, Typography } from "@material-ui/core";
 
 import { Add } from "@material-ui/icons";
 
-import { Button, InputSearch, InputSelect, AddAccountDialog } from "components";
+import { Button, InputSearch, InputSelect } from "components";
+
+const AddAccountDialog = lazy(() => import("../components/AddAccountDialog"));
 
 const sortOptions = [
   {
@@ -64,17 +66,17 @@ function AccountsPage({ accounts, fetchAccounts }) {
 
   return (
     <>
-      <Box bgcolor="#EFF0F2" p={2}>
+      <Box bgcolor='#EFF0F2' p={2}>
         <Grid container>
           <Grid item xs={12}>
             <Box
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
+              display='flex'
+              justifyContent='center'
+              alignItems='center'
               mb={2}
             >
-              <Typography variant="body1" component="div">
-                <Box fontWeight="normal" mr={3}>
+              <Typography variant='body1' component='div'>
+                <Box fontWeight='normal' mr={3}>
                   Sort by
                 </Box>
               </Typography>
@@ -91,34 +93,36 @@ function AccountsPage({ accounts, fetchAccounts }) {
         <Grid container spacing={2}>
           <Grid item xs={3}>
             <Box
-              bgcolor="#FAFBFC"
-              height="490px"
+              bgcolor='#FAFBFC'
+              height='490px'
               borderRadius={5}
               px={3}
               py={4}
             >
-              <Typography variant="h5" component="div">
-                <Box fontWeight="fontWeightBold" mb={3}>
+              <Typography variant='h5' component='div'>
+                <Box fontWeight='fontWeightBold' mb={3}>
                   Accounts
                 </Box>
               </Typography>
 
               <Button
-                text="Add"
-                icon={<Add fontSize="large" />}
+                text='Add'
+                icon={<Add fontSize='large' />}
                 fullWidth
-                color="#00aa70"
-                hover="#00915f"
+                color='#00aa70'
+                hover='#00915f'
                 pb={4}
                 onClick={() => setOpenDialog(true)}
               />
               {openDialog && (
-                <AddAccountDialog
-                  openDialog={openDialog}
-                  handleCloseDialog={() => setOpenDialog(false)}
-                />
+                <Suspense fallback={<div>Loading...</div>}>
+                  <AddAccountDialog
+                    openDialog={openDialog}
+                    handleCloseDialog={() => setOpenDialog(false)}
+                  />
+                </Suspense>
               )}
-              <Box bgcolor="#ffffff">
+              <Box bgcolor='#ffffff'>
                 <InputSearch
                   value={searchText}
                   onChangeValue={value => setSearchText(value)}
@@ -131,11 +135,11 @@ function AccountsPage({ accounts, fetchAccounts }) {
             {sortedAccount.map(item => (
               <Box borderRadius={5} mb={2} key={item.id}>
                 <Paper style={{ padding: 15 }} elevation={0}>
-                  <Grid container alignItems="center">
+                  <Grid container alignItems='center'>
                     <Grid item style={{ lineHeight: 0 }}>
                       <Box
                         bgcolor={item.bgcolor.value}
-                        color="white"
+                        color='white'
                         borderRadius={5}
                       >
                         {item.icon}
@@ -143,7 +147,7 @@ function AccountsPage({ accounts, fetchAccounts }) {
                     </Grid>
 
                     <Grid item xs={3}>
-                      <Typography variant="body1" component="div">
+                      <Typography variant='body1' component='div'>
                         <Box fontSize={18} fontWeight={500} pl={2}>
                           {item.name}
                         </Box>
@@ -152,19 +156,19 @@ function AccountsPage({ accounts, fetchAccounts }) {
 
                     <Grid item xs={4}>
                       <Typography
-                        variant="body1"
-                        component="div"
-                        color="textSecondary"
+                        variant='body1'
+                        component='div'
+                        color='textSecondary'
                       >
-                        <Box fontSize={15} fontWeight="fontWeightMedium">
+                        <Box fontSize={15} fontWeight='fontWeightMedium'>
                           {item.type.value}
                         </Box>
                       </Typography>
                     </Grid>
 
                     <Grid item xs={4}>
-                      <Typography variant="body1" component="div">
-                        <Box fontSize={18} fontWeight={500} textAlign="right">
+                      <Typography variant='body1' component='div'>
+                        <Box fontSize={18} fontWeight={500} textAlign='right'>
                           {item.currency.value} {item.balance}
                         </Box>
                       </Typography>
