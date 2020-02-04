ac3c86d33b8a70f3b52155aa0d6ab14dfd89a97d
Abdullah Jan <abdullahjanazeemi@gmail.com>
2020-02-04 16:44:53 +0500

    Apply HOC in Analytics Page

--- src/pages/AnalyticsPage.js
+++ src/pages/AnalyticsPage.js
@@ -1,15 +1,33 @@
 import React from "react";
 
-import { withStyles } from "@material-ui/core";
+import withDelayMount from "../HOC/withDelayMount";
+import withAccountsData from "../HOC/withAccountsData";
 
-const styles = theme => ({});
+const SimpleComponent = props => (
+  <div>
+    Component load
+    {props.wait || props.wait === 0 ? ` after ${props.wait}s` : " Immediately"}
+    {props.accounts && ` ${props.accounts.map(item => item.name)}`}
+  </div>
+);
+
+const EnhancedComponent = withDelayMount(30)(SimpleComponent);
+const EnhancedComponentwithData = withAccountsData(
+  withDelayMount(50)(SimpleComponent)
+);
 
 function AnalyticsPage({ classes }) {
   return (
     <>
       <h1>Analytics Page</h1>
+      <SimpleComponent />
+      <EnhancedComponent wait={0} />
+      {[...new Array(7).keys()].map(item => (
+        <EnhancedComponent key={item} wait={item + 1} />
+      ))}
+      <EnhancedComponentwithData wait={3} />
     </>
   );
 }
 
-export default withStyles(styles)(AnalyticsPage);
+export default AnalyticsPage;
