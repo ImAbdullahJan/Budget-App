79ce10a1cbb88259c8a6c45f8bebe9dc56928d97
Abdullah Jan <abdullahjanazeemi@gmail.com>
2020-03-06 13:02:58 +0500

    Customize Layout button for programmatic positioning and size

--- src/components/EditLayoutButton.js
+++ src/components/EditLayoutButton.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Fab } from "@material-ui/core";
 
@@ -7,17 +7,24 @@ import { Edit as EditIcon, Save as SaveIcon } from "@material-ui/icons";
 const useStyles = makeStyles(theme => ({
   root: {
     position: "absolute",
-    bottom: 20,
-    right: 20
+    top: props => props.top,
+    bottom: props => props.bottom,
+    right: props => props.right,
+    left: props => props.left
   }
 }));
 
-export default function FloatingActionButtons({ editLayout, onEditLayout }) {
-  const classes = useStyles();
+export default function EditLayoutButton(props) {
+  const classes = useStyles(props);
+  const { editLayout, onEditLayout, size } = props;
 
   return (
     <div className={classes.root}>
-      <Fab color='secondary' aria-label='edit' onClick={onEditLayout}>
+      <Fab
+        size={size}
+        aria-label='edit'
+        onClick={onEditLayout}
+      >
         {!editLayout ? <EditIcon /> : <SaveIcon />}
       </Fab>
     </div>
